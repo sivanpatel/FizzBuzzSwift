85a8a5ffbdaec6ad9b42bd01dca27188973a4073
Sivan Patel <sivan.patel@googlemail.com>
2015-08-25 14:32:26 +0100

    Initial Commit

---
+++ FizzBuzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by Sivan Patel on 25/08/2015.
+//  Copyright (c) 2015 Sivan Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
